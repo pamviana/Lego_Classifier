6ac5264a15de75cd30f9564a81bec21afa2c9c8f
Pam <pamellabviana@gmail.com>
2023-04-13 19:13:15 -0400

    adding python file

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
-from flask import Flask;
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
-def test_input(input){
-    if input%2 == 0:
-        return "even";
-    else :
-        return "odd"
+app = Flask(__name__)
+CORS(app)
+
+data = {
+    "result": 22,
+    "name": "Pamella"
 }
+
+# get  http://127.0.0.1:5000/result
+@app.route('/result', methods=['GET'])
+def disp():
+    return jsonify({'data': 100 ** 2})
+
+
+# driver function
+if __name__ == '__main__':
+    app.run(debug=True)
 
