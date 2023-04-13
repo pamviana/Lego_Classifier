ae981cf7420e844327dbb8f8f57c28045de590ce
Pam <pamellabviana@gmail.com>
2023-04-12 22:51:33 -0400

    starting web app

---
+++ index.js
@@ -0,0 +1,23 @@
+function openCamera(){
+    mainBox = document.getElementById("main_page");
+    mainBox.style.display = 'none';
+
+    cameraBox = document.getElementById("camera_page");
+    cameraBox.style.display = 'block';
+
+    startCamera();
+}
+
+async function startCamera(){
+    let video = document.querySelector("#video");
+    let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    video.srcObject = stream;
+}
+
+function takePicture(){
+    let canvas = document.querySelector("#canvas");
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    let image_data_url = canvas.toDataURL('image/jpeg');
+
+    console.log(image_data_url);
+}
