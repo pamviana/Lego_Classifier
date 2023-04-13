d11e38d6f104fec3bbe1752e88e0e8cd244f3eb5
Pam <pamellabviana@gmail.com>
2023-04-13 18:26:28 -0400

    did some modifications

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let stream; //not best practice, but this application is very small
 
 function openCamera(){
-    mainBox = document.getElementById("main_page");
+    let mainBox = document.getElementById("main_page");
     mainBox.style.display = 'none';
 
-    cameraBox = document.getElementById("camera_page");
+    let cameraBox = document.getElementById("camera_page");
     cameraBox.style.display = 'block';
 
     startCamera();
@@ -21,42 +21,48 @@ function takePicture(){
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
     let image_data_url = canvas.toDataURL('image/jpeg');
 
-    console.log(image_data_url);
+    openResultScreen(image_data_url, "camera_page");
 }
 
 function goBackToMain(currentScreen) {
     let screen = document.getElementById(currentScreen);
     screen.style.display = 'none';
 
-    mainBox = document.getElementById("main_page");
+    let mainBox = document.getElementById("main_page");
     mainBox.style.display = 'block';
 
-    if(currentScreen == 'camera_page') {
+    if(currentScreen == 'camera_page' && stream != undefined) {
         stream.getTracks()[0].stop();
     }
 }
 
-function uploadPicture(){
-    mainBox = document.getElementById("main_page");
-    mainBox.style.display = 'none'; 
+function openResultScreen(picture, previousScreen){
+    let pictureOutput = document.getElementById('output');
+    pictureOutput.style.display = "block";
+    pictureOutput.innerHTML = '<img src="' + picture + '" style="height: 100%; width: 80%; object-fit: cover;"/>';
 
-    cameraBox = document.getElementById("upload_page");
+    let resultText = document.getElementById('result-text');
+    resultText.innerHTML = "Harry Potter minifigure";
+        
+    let previousContent = document.getElementById(previousScreen);
+    previousContent.style.display = 'none'; 
+
+    let cameraBox = document.getElementById("upload_page");
     cameraBox.style.display = 'block';
+
 }
 
-const pictureOutput = document.getElementById('output');
-const file =document.getElementById('file');
+
+// Listener for when "Upload a picture" button is pressed
+const file = document.getElementById('file');
 
 file.addEventListener('change', function(){
-
     const files = file.files[0];
     if (files) {
     const fileReader = new FileReader();
     fileReader.readAsDataURL(files);
     fileReader.addEventListener("load", function () {
-    pictureOutput.style.display = "block";
-    pictureOutput.innerHTML = '<img src="' + this.result + '" style="height: 100%; width: 80%; object-fit: cover;"/>';
-    uploadPicture();
+        openResultScreen(this.result, "main_page");
     });    
   }
 });
---
+++ test.py
@@ -0,0 +1,6 @@
+def test_input(input){
+    if input%2 == 0:
+        return "even";
+    else :
+        return "odd"
+}
